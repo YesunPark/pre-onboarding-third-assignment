087cfc56a4b6d07a85cebff809e7b6919b8c6c7a
kkukileon305 <kkukileon305@gmail.com>
2022-10-11 19:53:10 +0900

    [ADD]: record기능 추가

--- src/pages/record/Record.jsx
+++ src/pages/record/Record.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from 'react';
+
 const Record = () => {
+  const [isRecording, setIsRecording] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+      });
+
+      const recorder = new MediaRecorder(stream);
+    })();
+  }, []);
+
   return <div>asd</div>;
 };
 
