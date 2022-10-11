d0e7638afbee48cb6fa96e843e086f52c8bd18e3
kkukileon305 <kkukileon305@gmail.com>
2022-10-11 22:48:30 +0900

    [ADD]녹음페이지 녹음기능, 재생기능 추가, 유저가 마이크 사용을 거부하였을 때 에러처리 해야함

--- src/pages/record/Record.jsx
+++ src/pages/record/Record.jsx
@@ -1,19 +1,87 @@
-import { useEffect, useState } from 'react';
+import { useRef, useState } from 'react';
+import { BsPlayFill, BsSquareFill } from 'react-icons/bs';
+import styled from 'styled-components';
 
 const Record = () => {
   const [isRecording, setIsRecording] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-      });
+  const mediaRecorderRef = useRef();
+  const audioArray = useRef([]);
+  const [audioURL, setAudioURL] = useState('');
+  const audioElement = useRef(null);
 
-      const recorder = new MediaRecorder(stream);
-    })();
-  }, []);
+  const [timeMaxValue, setTimeMaxValue] = useState(0);
+  const debounce = useRef(0);
 
-  return <div>asd</div>;
+  const toggleRecord = async () => {
+    if (!isRecording) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        setIsRecording(true);
+
+        const recorder = new MediaRecorder(stream);
+
+        recorder.ondataavailable = ({ data }) => audioArray.current.push(data);
+
+        recorder.onstop = () => {
+          const blob = new Blob(audioArray.current);
+
+          audioArray.current.splice(0);
+
+          setAudioURL(window.URL.createObjectURL(blob));
+          setIsRecording(false);
+        };
+
+        recorder.start();
+        mediaRecorderRef.current = recorder;
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      mediaRecorderRef.current.stop();
+    }
+  };
+
+  const startAudio = () => {
+    if (audioURL) {
+      setIsPlaying(true);
+      audioElement.current.play();
+    }
+  };
+
+  const changeHandler = ({ target: { value } }) => {
+    clearTimeout(debounce.current);
+    debounce.current = setTimeout(() => setTimeMaxValue(value), 300);
+  };
+
+  return (
+    <StyledRecord>
+      <p>최대 녹음 시간 {timeMaxValue}s</p>
+      <input type='range' onChange={changeHandler} max={100} min={10} />
+      <audio ref={audioElement} src={audioURL} onEnded={() => setIsPlaying(false)} />
+      <div className='buttonContainer'>
+        <button onClick={toggleRecord}>{isRecording ? <BsSquareFill /> : <BsPlayFill />}</button>
+        <button onClick={startAudio}>재생</button>
+      </div>
+      <p>{isPlaying ? '재생중입니다' : '재생할까요'}</p>
+    </StyledRecord>
+  );
 };
+
+const StyledRecord = styled.div`
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div.buttonContainer {
+    display: flex;
+  }
+
+  input {
+    width: 100%;
+  }
+`;
 
 export default Record;
