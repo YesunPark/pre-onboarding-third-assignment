1e5877aaeff8ed0e220f7ad4871da3f69bb7162e
YesunPark <lynn08082@gmail.com>
2022-10-12 00:28:36 +0900

    [Add]: 플레이페이지 기본틀 생성

--- src/pages/play/Play.jsx
+++ src/pages/play/Play.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import styled from 'styled-components';
+
+recordPlay = new MediaPlayer();
 
 const Play = () => {
-  return <div>Play</div>;
+  //음성이 녹음된 후 재생컴포넌트 만들기
+  //재생 버튼 클릭 시 재생화면나오게
+  // navigator.mediaDevices.getUserMedia({ audio: true });
+
+  return <PlayContainer>Play</PlayContainer>;
 };
+
+const PlayContainer = styled.div``;
 
 export default Play;
