d8e70b15961b0b988dc5d68ec21cb7959251e69e
kkukileon305 <kkukileon305@gmail.com>
2022-10-12 16:28:40 +0900

    [Add]: playList페이지 뒤로가기 버튼 추가

--- src/components/play/PlayList.jsx
+++ src/components/play/PlayList.jsx
@@ -4,6 +4,9 @@ const PlayList = ({ storageRef }) => {
   return <StyledList>{storageRef.name}</StyledList>;
 };
 
-const StyledList = styled.li``;
+const StyledList = styled.li`
+  padding: 20px 0;
+  border-bottom: 1px solid white;
+`;
 
 export default PlayList;
--- src/pages/play/Play.jsx
+++ src/pages/play/Play.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
-import { BsFillRecordFill, BsSquareFill } from 'react-icons/bs';
+import { AiFillPlayCircle, AiOutlineArrowLeft } from 'react-icons/ai';
 import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
 import styled from 'styled-components';
 import storage from '../../firebase/storage';
 import PlayList from '../../components/play/PlayList';
+import { useNavigate } from 'react-router-dom';
 
 const Play = () => {
+  const navigate = useNavigate();
+
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -21,13 +24,40 @@ const Play = () => {
 
   return (
     <Container>
+      <Title>
+        <AiOutlineArrowLeft size={20} onClick={() => navigate('/')} />
+        <h2>재생목록</h2>
+      </Title>
       <ul>{audioList && audioList.map(storageRef => <PlayList key={storageRef.name} storageRef={storageRef} />)}</ul>
-      <PlayBar>플레이바</PlayBar>
+      <PlayBar>
+        <AiFillPlayCircle size={40} />
+      </PlayBar>
     </Container>
   );
 };
 
-const Container = styled.div``;
+const Title = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  z-index: 10;
+  width: 100%;
+  max-width: 600px;
+  background-color: #3eacac;
+  border-bottom: 1px solid white;
+`;
+
+const Container = styled.div`
+  padding-top: 20px;
+
+  ul {
+    margin-top: 40px;
+  }
+`;
 
 const PlayBar = styled.div`
   position: fixed;
@@ -39,7 +69,9 @@ const PlayBar = styled.div`
   align-items: center;
   justify-content: center;
   gap: 20px;
-  padding: 20px;
+  padding: 10px;
+  border-top: 1px solid white;
+  background-color: #3eacac;
 `;
 
 export default Play;
